a855552344545eabfca7c8224c8f1256b133b113
UrbanSphere <>
2022-09-10 18:07:50 +0200

    added more methods to Recipe class

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from models import *
 
-
-Mleko = Product('mleko', 'nabiał', 'ml')
-Mleko.add_to_db()
-Chleb = Product('chleb', 'pieczywo', 'szt')
-Chleb.add_to_db()
-print(Product.show_all_products())
-
-Chleb.del_from_db()
-print(Product.show_all_products())
 
 
--- models.py
+++ models.py
@@ -76,3 +76,42 @@ class Recipe:
         self.ingr = ingredients
 
 
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM recipes WHERE name=?'
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        if row:
+            return cls(row[0], row[1], row[2], row[3])
+        else:
+            return None
+
+
+    def add_to_db(self):
+        if self.find_by_name(self.name):
+            self.del_from_db()
+
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+
+        query = 'INSERT INTO recipes VALUES (?, ?, ?, ?)'
+        cursor.execute(query, (self.name, self.cat, self.des, self.ingr))
+
+        connection.commit()
+        connection.close()
+
+
+    def del_from_db(self):
+        if self.find_by_name(self.name) is None:
+            raise NameError(f'No such product called \'{self.name}\' to be deleted')
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+
+        query = 'DELETE FROM recipes WHERE name=?'
+        cursor.execute(query, (self.name,))
+
+        connection.commit()
+        connection.close()
