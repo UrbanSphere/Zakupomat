90c280bb03b7e8b717be133c74168036a94267c0
UrbanSphere <>
2022-09-10 18:07:18 +0200

    corrected table creation

--- create_tables.py
+++ create_tables.py
@@ -6,7 +6,8 @@ cursor = connection.cursor()
 create_products_table = 'CREATE TABLE IF NOT EXISTS products (name text, category text, unit text, amount real)'
 cursor.execute(create_products_table)
 
-create_recipes_table = 'CREATE TABLE IF NOT EXISTS recipes (description text, blob products)'
+create_recipes_table = 'CREATE TABLE IF NOT EXISTS recipes' \
+                       ' (name text, category text, description text, ingredients blob)'
 cursor.execute(create_recipes_table)
 
 connection.commit()
